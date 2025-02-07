c3c0af0b5f9984f0aa55379c24b96ab1a06b82b7
Yehia Elrify <137741022+YehiaElrify1@users.noreply.github.com>
2025-02-07 21:08:24 +0200

    Create simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Calculate simple interest
+echo "Enter principal amount:"
+read principal
+echo "Enter rate of interest:"
+read rate
+echo "Enter time period (in years):"
+read time
+interest=$((principal * rate * time / 100))
+echo "Simple Interest: $interest"
